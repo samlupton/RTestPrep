9984130ccb4d6d3938f32594bfded41148485023
Tanner Benbrook <Tanner.M.Benbrook-1@ou.edu>
2024-10-09 15:57:53 -0500

    type shit

--- testPrep.R
+++ testPrep.R
@@ -28,3 +28,52 @@ att
 #P(A|B) = (P(A)P(B|A))/P(B)
 
 #posterior, prior p(A), the likelihood(p(B|A)), and marginal(denominator)
+
+getwd()
+#gives option to find document
+ddt <- read.csv(file.choose())
+head(ddt,3)
+
+ddt[c("LENGTH", "WEIGHT")]
+#$ means column
+#the comma just means that it is searching for every row, 
+#and it will give you every column with that given argument
+ddtcat <- ddt[ddt$SPECIES == "CCATFISH",]
+ddtcat
+#names of variables inside of ddt table(qualitative and quantitative)
+names(ddt)
+#summary inspects the class of the ddt dataframe object
+summary(ddt)
+
+ddt[ddt$LENGTH > 48 & ddt$WEIGHT > 1200,]
+#searches through the ddt dataframe and if a row contains lmbass or trm,
+#it returns all of the columns that meets the arguments.
+ddt[ddt$SPECIES == "LMBASS" | ddt$RIVER == "TRM",]
+
+library(dplyr)
+#puts the ddt argument into filter by piping(pause)
+dplyrcat <- ddt %>% filter(SPECIES == "CCATFISH")
+dplyrcat
+
+#practice on how to use pipelining
+
+#1
+ddt %>% filter(SPECIES == "SMBUFFALO") %>% summarize(mean_WEIGHT = mean(WEIGHT))
+#2
+ddt %>% filter(SPECIES == "CCATFISH" & DDT > 33) %>% summarize(mean_LENGTH = mean(LENGTH))
+
+ddt%>% filter(SPECIES == "LMBASS" | SPECIES == "SMBUFFALO") %>% summarize(sd_DDT = sd(DDT))
+
+ddt %>% filter(LENGTH > 40 & WEIGHT > 1000)
+
+ddt %>% filter(SPECIES == "LMBASS" & LENGTH > 30)
+
+ddt %>% filter(LENGTH > 49 & LENGTH < 50)
+
+ddt %>% filter(LENGTH > 49 & WEIGHT < 1200)
+
+l <-ddt$WEIGHT
+z <- (l-mean(l))/sd(l)
+z
+#how to find outliers
+l[abs(z) > 3]
